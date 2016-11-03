c21bad37440f73317ca0b4b32ba1e58a0825046c
Martin Desharnais <martin.desharnais@gmail.com>
2016-11-03 09:59:02 +0100

    Type inference v2 (kind of working but not)

---
+++ src/FreshName.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving, UndecidableInstances, TypeSynonymInstances, FlexibleInstances #-}
+
+module FreshName where
+
+import Control.Monad.State
+import Control.Monad.Identity
+
+type Name = String
+
+class Monad m => MonadNameGen m where
+  fresh :: m Name
+
+newtype NameGenT m a = NameGenT (StateT [Name] m a)
+  deriving (Applicative, Functor, Monad, MonadIO, MonadTrans)
+
+type NameGen a = NameGenT Identity a
+
+runNameGen :: NameGen a -> a
+runNameGen = runIdentity . runNameGenT
+
+instance (Monad m) => MonadNameGen (NameGenT m) where
+  fresh = NameGenT $ do t:ts <- get; put ts; return t
+
+runNameGenT :: (Monad m) => NameGenT m a -> m a
+runNameGenT (NameGenT x) =
+   evalStateT x ["x" ++ (show i) | i <- [(0::Int)..]]
+
+foo :: NameGen String
+foo = do
+  n1 <- fresh
+  n2 <- fresh
+  return (n1 ++ " " ++ n2)
+
+bar :: NameGenT IO String
+bar = do
+  n1 <- fresh
+  n2 <- fresh
+  lift $ putStrLn "Test"
+  return (n1 ++ " " ++ n2)
+
+baz :: NameGenT Maybe String
+baz = do
+  n1 <- fresh
+  n2 <- fresh
+  lift (do
+    x <- Just "~~~"
+    return (n1 ++ x ++ n2))
--- src/Type.hs
+++ src/Type.hs
@@ -1,6 +1,8 @@
 module Type where
 
 import Parser(Term(..))
+import FreshName
+import Control.Monad.Trans(lift)
 
 data Type =
   TBool |
@@ -9,18 +11,23 @@ data Type =
   TVar String
   deriving (Eq, Show)
 
-type Context = String -> Type
+type Context a = String -> NameGenT a Type
 
-extendContext :: Context -> String -> Type -> Context
-extendContext c s t = \x -> if x == s then t else c s
+emptyContext :: Monad a => Context a
+emptyContext = const $ fresh >>= \x -> return (TVar x)
+
+extendContext :: Monad a => Context a -> String -> Type -> Context a
+extendContext c s t = \x ->  if x == s then return t else c s
 
 type Subst = Type -> Type
 
 concat :: Subst -> Subst -> Subst
 concat a b = b . a
 
-apply :: Subst -> Context -> Context
-apply s c = s . c
+apply :: Monad a => Subst -> Context a -> Context a
+apply s c x = do
+  tau <- c x
+  return (s tau)
 
 unify :: Type -> Type -> Maybe Subst
 unify TInt TInt = Just id
@@ -38,33 +45,25 @@ unify (TFun t1 t2) (TFun t1' t2') =
       Just (Type.concat theta1 theta2)
 unify _ _ = Nothing
 
-checkBinOpElements :: Type -> Context -> Term -> Term -> Maybe (Subst, Type)
-checkBinOpElements t c e1 e2 = do
-  (theta1, tau1) <- infer c e1
-  if tau1 == t then
-    do (theta2, tau2) <- infer (apply theta1 c) e2
-       if tau2 == t then
-         Just (Type.concat theta1 theta2, TInt)
-       else
-         Nothing
-  else
-    Nothing
-
-infer :: Context -> Term -> Maybe (Subst, Type)
-infer c (Var x) = Just (id, c x)
+infer :: Context Maybe -> Term -> NameGenT Maybe (Subst, Type)
+infer c (Var x) = do
+  tau <- c x
+  return (id, tau)
 infer c (Abs x e) = do
-  let alpha = TVar "alpha"
+  name <- fresh
+  let alpha = TVar name
   (theta, tau) <- infer (extendContext c x alpha) e
   return (theta, TFun (theta alpha) tau)
 infer c (App e1 e2) = do
   (theta1, tau1) <- infer c e1
   (theta2, tau2) <- infer (apply theta1 c) e2
-  let beta = TVar "beta"
-  theta3 <- unify (theta2 tau1) (TFun tau2 beta)
+  name <- fresh
+  let beta = TVar name
+  theta3 <- lift $ unify (theta2 tau1) (TFun tau2 beta)
   return (theta1 `Type.concat` theta2 `Type.concat` theta3, theta3 beta)
-infer c (LitInt _) = Just (id, TInt)
-infer c LitTrue = Just (id, TBool)
-infer c LitFalse = Just (id, TBool)
+infer c (LitInt _) = return (id, TInt)
+infer c LitTrue = return (id, TBool)
+infer c LitFalse = return (id, TBool)
 infer c (OpMul e1 e2) = checkBinOpElements TInt c e1 e2
 infer c (OpDiv e1 e2) = checkBinOpElements TInt c e1 e2
 infer c (OpAdd e1 e2) = checkBinOpElements TInt c e1 e2
@@ -74,13 +73,13 @@ infer c (OpEQ e1 e2) = checkBinOpElements TInt c e1 e2
 infer c (If e e1 e2) = do
   (theta, tau) <- infer c e
   if tau /= TBool then
-    Nothing
+    lift $ Nothing
   else do
     let c' = apply theta c
     (theta1, tau1) <- infer c' e1
     let c'' = apply theta1 c'
     (theta2, tau2) <- infer c'' e2
-    theta3 <- unify (theta2 tau1) tau2
+    theta3 <- lift $ unify (theta2 tau1) tau2
     let cat = Type.concat
     return (theta `cat` theta1 `cat` theta2 `cat` theta3, theta3 tau2)
 infer c (Let x e1 e2) = do
@@ -88,7 +87,21 @@ infer c (Let x e1 e2) = do
   (theta2, tau2) <- infer (extendContext (apply theta1 c) x tau1) e2
   return (theta1 `Type.concat` theta2, tau2)
 infer c (LetRec x e1 e2) = do
-  let alpha = TVar "alpha"
+  name <- fresh
+  let alpha = TVar name
   (theta1, tau1) <- infer (extendContext c x alpha) e1
   (theta2, tau2) <- infer (extendContext (apply theta1 c) x tau1) e2
   return (theta1 `Type.concat` theta2, tau2)
+
+checkBinOpElements :: Type -> Context Maybe -> Term -> Term ->
+  NameGenT Maybe (Subst, Type)
+checkBinOpElements t c e1 e2 = do
+  (theta1, tau1) <- infer c e1
+  if tau1 == t then
+    do (theta2, tau2) <- infer (apply theta1 c) e2
+       if tau2 == t then
+         return (Type.concat theta1 theta2, TInt)
+       else
+         lift $ Nothing
+  else
+    lift $ Nothing
