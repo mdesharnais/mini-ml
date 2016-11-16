c84be7c1fcccb6f14f2afcbb7db38c20b29abba5
Martin Desharnais <martin.desharnais@gmail.com>
2016-11-16 22:51:56 +0100

    Add bool and op. to normal form test

--- src/Test.hs
+++ src/Test.hs
@@ -6,9 +6,16 @@ type Id = String
 
 -- Source language
 data Expr =
-  ELit Integer |
+  ELitInt Integer |
+  ELitBool Bool |
   EVar Id |
-  EPlus Expr Expr |
+  EOpAdd Expr Expr |
+  EOpSub Expr Expr |
+  EOpMul Expr Expr |
+  EOpDiv Expr Expr |
+  EOpLT Expr Expr |
+  EOpEQ Expr Expr |
+  EIf Expr Expr Expr |
   ELet Id Expr Expr |
   EAbs Id Expr |
   EApp Expr Expr
@@ -16,11 +23,18 @@ data Expr =
 
 -- Intermediate language
 data AtomicTerm =
-  ATLit Integer |
+  ATLitInt Integer |
+  ATLitBool Bool |
   ATVar Id
 
 data SimpleTerm =
-  STPlus AtomicTerm AtomicTerm |
+  STOpAdd AtomicTerm AtomicTerm |
+  STOpSub AtomicTerm AtomicTerm |
+  STOpMul AtomicTerm AtomicTerm |
+  STOpDiv AtomicTerm AtomicTerm |
+  STOpLT AtomicTerm AtomicTerm |
+  STOpEQ AtomicTerm AtomicTerm |
+  STIf AtomicTerm Term Term |
   STAbs Id Term |
   STApp AtomicTerm AtomicTerm
 
@@ -30,11 +44,19 @@ data Term =
 
 -- Pretty print
 instance Show AtomicTerm where
-  show (ATLit n) = show n
+  show (ATLitInt n) = show n
+  show (ATLitBool n) = show n
   show (ATVar x) = x
 
 instance Show SimpleTerm where
-  show (STPlus e1 e2) = show e1 ++ " + " ++ show e2
+  show (STOpAdd e1 e2) = show e1 ++ " + " ++ show e2
+  show (STOpSub e1 e2) = show e1 ++ " - " ++ show e2
+  show (STOpMul e1 e2) = show e1 ++ " * " ++ show e2
+  show (STOpDiv e1 e2) = show e1 ++ " / " ++ show e2
+  show (STOpLT e1 e2) = show e1 ++ " < " ++ show e2
+  show (STOpEQ e1 e2) = show e1 ++ " = " ++ show e2
+  show (STIf e1 e2 e3) =
+    "if " ++ show e1 ++ " then " ++ show e2 ++ " else " ++ show e3
   show (STAbs x e) = "fun " ++ x ++ " -> " ++ show e
   show (STApp e1 e2) = show e1 ++ " " ++ show e2
 
@@ -43,13 +65,30 @@ instance Show Term where
   show (TLet x e1 e2) = "let " ++ x ++ " = " ++ show e1 ++ " in\n" ++ show e2
 
 -- Normal form
-nf :: Expr -> (Id -> AtomicTerm) -> (AtomicTerm -> NameGen Term) -> NameGen Term
-nf (ELit n) s k = k (ATLit n)
-nf (EVar x) s k = k (s x)
-nf (EPlus e1 e2) s k = nf e1 s (\a -> nf e2 s (\b -> do
+
+nfBinOp :: Expr -> Expr -> (AtomicTerm -> AtomicTerm -> SimpleTerm) ->
+  (Id -> AtomicTerm) -> (AtomicTerm -> NameGen Term) -> NameGen Term
+nfBinOp e1 e2 op s k = nf e1 s (\a -> nf e2 s (\b -> do
   c <- fresh
   d <- k (ATVar c)
-  return (TLet c (STPlus a b) d)))
+  return (TLet c (op a b) d)))
+
+nf :: Expr -> (Id -> AtomicTerm) -> (AtomicTerm -> NameGen Term) -> NameGen Term
+nf (ELitInt n) s k = k (ATLitInt n)
+nf (ELitBool b) s k = k (ATLitBool b)
+nf (EVar x) s k = k (s x)
+nf (EOpAdd e1 e2) s k = nfBinOp e1 e2 STOpAdd s k
+nf (EOpSub e1 e2) s k = nfBinOp e1 e2 STOpSub s k
+nf (EOpMul e1 e2) s k = nfBinOp e1 e2 STOpMul s k
+nf (EOpDiv e1 e2) s k = nfBinOp e1 e2 STOpDiv s k
+nf (EOpLT e1 e2) s k = nfBinOp e1 e2 STOpLT s k
+nf (EOpEQ e1 e2) s k = nfBinOp e1 e2 STOpEQ s k
+nf (EIf e1 e2 e3) s k = nf e1 s (\e1' -> do
+  e2' <- nf e2 s (return . TVal)
+  e3' <- nf e3 s (return . TVal)
+  a <- fresh
+  b <- k (ATVar a)
+  return (TLet a (STIf e1' e2' e3') b))
 nf (ELet x e1 e2) s k = nf e1 s (\a -> nf e2 (\y -> if y == x then a else ATVar y) (return . TVal))
 nf (EAbs x e) s k = do
   a <- fresh
