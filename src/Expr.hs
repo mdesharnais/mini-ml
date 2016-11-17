b25a1dc33d52cb16b84f6dc8c49f1a041c8f4edf
Martin Desharnais <martin.desharnais@gmail.com>
2016-11-17 14:17:00 +0100

    Add pretty print to `Expr`

--- src/Expr.hs
+++ src/Expr.hs
@@ -1,6 +1,5 @@
 module Expr where
 
-
 type Id = String
 
 data Expr =
@@ -18,4 +17,23 @@ data Expr =
   LetRec Id (Id, Expr) Expr |
   Abs Id Expr |
   App Expr Expr
-  deriving (Eq, Show)
+  deriving (Eq)
+
+instance Show Expr where
+  show (LitInt n) = show n
+  show (LitBool b) = show b
+  show (Var x) = x
+  show (OpAdd e1 e2) = show e1 ++ " + " ++ show e2
+  show (OpSub e1 e2) = show e1 ++ " - " ++ show e2
+  show (OpMul e1 e2) = show e1 ++ " * " ++ show e2
+  show (OpDiv e1 e2) = show e1 ++ " / " ++ show e2
+  show (OpLT e1 e2) = show e1 ++ " < " ++ show e2
+  show (OpEQ e1 e2) = show e1 ++ " = " ++ show e2
+  show (If e1 e2 e3) =
+    "if " ++ show e1 ++ " then " ++ show e2 ++ " else " ++ show e3
+  show (Let x e1 e2) =
+    "let " ++ x ++ " = " ++ show e1 ++ " in " ++ show e2
+  show (LetRec f (x, e1) e2) =
+    "let rec " ++ f ++ " = fun " ++ x ++ " -> " ++ show e1 ++ " in " ++ show e2
+  show (Abs x e1) = "(fun " ++ x ++ " -> " ++ show e1 ++ ")"
+  show (App e1 e2) = show e1 ++ " " ++ show e2
