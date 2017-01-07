8b9c0e79cd7e7c3ffc5e630c1ffd957b297b5486
Martin Desharnais <martin.desharnais@gmail.com>
2017-01-07 17:18:44 +0100

    Add type checking to compilation

--- app/Main.hs
+++ app/Main.hs
@@ -10,11 +10,13 @@ import qualified Type
 compileFile :: String -> String
 compileFile code =
   let tokens = Lexer.alexScanTokens code
-      prog = Parser.parse tokens
-      nf = Compiler.toNormalForm prog
-      nfCl = Compiler.toClosure nf
-      llvmIr = Compiler.Llvm.compile nfCl
-   in llvmIr
+      exp = Parser.parse tokens
+   in case Type.inferProgType exp of
+        Nothing -> "Does not type check"
+        Just ty -> let nf = Compiler.toNormalForm exp
+                       nfCl = Compiler.toClosure nf
+                       llvmIr = Compiler.Llvm.compile nfCl
+                    in llvmIr
 
 main :: IO ()
 main = interact compileFile
